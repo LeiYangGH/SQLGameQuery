e6c16f68457caff071a02cf22a7587768cf69bba
Lei Yang <675686066@qq.com>
2016-11-09 22:33:16 +0800

    order by regt

--- GameQuery.sql
+++ GameQuery.sql
@@ -11,16 +11,13 @@ v.maxrank
  from
 (
 select u.U_ID,
---gs.G_ID,
 (select top 1 G_ID from T_GAME_SCORE ti where ti.G_RANK = max(gs.G_RANK)) as gid,
+(select top 1 REG_TIME from T_USER_INFO ui where ui.U_ID = u.U_ID) as regt,
 max(gs.G_RANK) as maxrank
 from T_USER u
 left join T_GAME_SCORE gs on gs.U_ID = u.U_ID
-group by u.U_ID
+group by u.U_ID 
 ) as v 
 left join T_USER_INFO ui on ui.U_ID = v.U_ID
 left join T_GAME_INFO gi on gi.G_ID = v.gid
---order by ui.REG_TIME desc
---left join T_GAME_SCORE gs on gs.U_ID = u.U_ID
---left join T_GAME_INFO gi on gi.G_ID = gs.G_ID
---left join T_USER_INFO ui on ui.U_ID = u.U_ID
+order by v.regt desc
